314646585f63705a5764fde698d942dfc5120942
MahiTef <mahletteferi9@gmail.com>
2021-11-22 13:15:32 -0800

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,20 +7,20 @@
  */
 int is_palindrome(listint_t **head)
 {
-  size_t len, i;
-  listint_t *node1, *node2;
+        size_t len, i;
+	listint_t *node1, *node2;
 
-  if (!head || !(*head))
-    return (1);
-  len = listint_len(*head);
-  for (i = 0; i < (len / 2); i++)
-    {
-      node1 = get_nodeint_at_index(*head, i);
-      node2 = get_nodeint_at_index(*head, (len - 1 - i));
-      if (node1->n != node2->n)
-	return (0);
-    }
-  return (1);
+	if (!head || !(*head))
+	        return (1);
+	len = listint_len(*head);
+	for (i = 0; i < (len / 2); i++)
+	{
+	        node1 = get_nodeint_at_index(*head, i);
+		node2 = get_nodeint_at_index(*head, (len - 1 - i));
+		if (node1->n != node2->n)
+		        return (0);
+	}
+	return (1);
 }
 
 /**
@@ -30,16 +30,16 @@ int is_palindrome(listint_t **head)
  */
 size_t listint_len(listint_t *h)
 {
-  listint_t *tmp;
-  size_t count = 0;
+        listint_t *tmp;
+	size_t count = 0;
 
-  tmp = h;
-  while (tmp)
-    {
-      count++;
-      tmp = tmp->next;
-    }
-  return (count);
+	tmp = h;
+	while (tmp)
+	{
+	        count++;
+		tmp = tmp->next;
+	}
+	return (count);
 }
 
 /**
@@ -50,19 +50,19 @@ size_t listint_len(listint_t *h)
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-  unsigned int i = 0;
-  listint_t *nth;
+        unsigned int i = 0;
+	listint_t *nth;
 
-  nth = head;
-  while (i < index)
-    {
-      nth = nth->next;
-      i++;
+	nth = head;
+	while (i < index)
+	{
+	        nth = nth->next;
+		i++;
 
-      if (!nth)
-	return (0);
+		if (!nth)
+		        return (0);
 
-    }
+	}
 
-  return (nth);
+	return (nth);
 }
