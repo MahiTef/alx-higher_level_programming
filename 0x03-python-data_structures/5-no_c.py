0567f327b3996af235056556b729132bcb4ea0ca
MahiTef <mahletteferi9@gmail.com>
2021-11-22 20:29:48 +0000

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     copy = ""
     for i in range(0, len(my_string)):
-        if my_strint[i] != 'c' and my_strint[i] != 'C':
-            copy += my_strint[i]
+        if my_string[i] != 'c' and my_string[i] != 'C':
+            copy += my_string[i]
     return copy
