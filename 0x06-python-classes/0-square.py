1adf242b1044b4e7300dabe5f6d2bb2b91e69709
MahiTef <mahletteferi9@gmail.com>
2021-12-08 10:20:09 -0800

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""And empty square class."""
+
+
 class Square:
-    """ Square class is an empty class that defines a square """
+    """And empty square class with an artribute"""
     pass
